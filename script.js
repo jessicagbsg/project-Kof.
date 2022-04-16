f4829ca4b0729804f82d058bdb6392e64fb6a0e9
Jessica Gondim <jessicagbsg@gmail.com>
2022-04-16 00:12:02 -0300

    small adjustments

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const botaoMenu = document.querySelector(".header__menu-items--pop-up-button");
+const menuButton = document.querySelector(".header__menu-items--pop-up-button");
 const menu = document.querySelector(".header__menu-items--pop-up");
 
-botaoMenu.addEventListener("click", () => {
+menuButton.addEventListener("click", () => {
   menu.classList.toggle("header__menu-items--pop-up-enable");
+});
+
+window.addEventListener("scroll", () => {
+  menu.classList.remove("header__menu-items--pop-up-enable");
 });
